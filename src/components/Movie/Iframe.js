93cf0b932c35b5dce509cf1644e4a3df09053344
milos mijakovac <milosmijakovac@gmail.com>
2021-10-11 14:15:50 +0200

    iframe added

---
+++ src/components/Movie/Iframe.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Iframe = () => {
+  return (
+    <div>
+      <h1>test Iframe</h1>
+      <iframe src="http://arty-web-staging-3pig4.ondigitalocean.app/services/api/model-viewer/63a55bf3-c96c-4f89-a65e-f131b801e73a/?model=e83b6793-5151-47a1-8302-cc7bcb52420a" style="width:100%; height:600px; border: none"></iframe>
+    </div>
+  )
+}
+
+export default Iframe
